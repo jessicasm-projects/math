3a4fea5a20d75c54423f7d87b3568a0234a1e85e
Jessica Madamba <jessica.madamba@gmail.com>
2019-07-17 01:37:13 -0700

    Implement form component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Form from './Form';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      operation: 'simplify',
+      equation: '',
+      result: '',
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    let newState = {};
+    newState[event.target.name] = event.target.value;
+    this.setState(newState);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div>
+        <Form onChange={this.handleChange} onSubmit={this.handleSubmit} operation={this.state.operation} equation={this.state.equation} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Form.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Form = (props) => (
+  <form>
+    <label>
+      Operation:
+      <select name="operation" value={props.operation} onChange={props.onChange}>
+        <option value="simplify">Simplify</option>
+        <option value="factor">Factor</option>
+        <option value="derive">Derive</option>
+        <option value="integrate">Integrate</option>
+        <option value="zeros">Find O's</option>
+        <option value="tangent">Find Tangent</option>
+        <option value="area">Area Under Curve</option>
+        <option value="cos">Cosine</option>
+        <option value="sin">Sine</option>
+        <option value="tan">Tangent</option>
+        <option value="arccos">Inverse Cosine</option>
+        <option value="arcsin">Inverse Sine</option>
+        <option value="arctan">Inverse Tangent</option>
+        <option value="abs">Absolute Value</option>
+        <option value="log">Logarithm</option>
+      </select>
+    </label>
+    <label>
+      Equation:
+      <input name="equation" value={props.equation} onChange={props.onChange} />
+    </label>
+    <button onClick={props.onSubmit}>Submit</button>
+  </form>
+);
+
+export default Form;
